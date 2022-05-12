9001d3bf6e6e86a5f50a67ca8fcaa20aa92fa420
Nazim Filzer <nazimfilzer@gmail.com>
2022-05-12 14:54:59 +0530

    Stable version 2 where i created Users route

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ db.connect();
 router.get('/admin', async (req, res, next) => {
 
   let products = await db.get().collection("items").find().toArray();
-  res.render('index', { products });
+  res.render('index', { products,style:"style.css" });
 
 });
 
@@ -18,6 +18,11 @@ router.post('/admin',async(req,res)=>{
     console.log("Job Details Pushed");
   })
   res.render('success',{name:req.body.name,role:req.body.role,company:req.body.company,desc:req.body.desc})
+
+})
+
+router.get('/admin/edit', async(req,res)=>{
+  res.send("edit page")
 })
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,15 @@
 var express = require('express');
+const async = require('hbs/lib/async');
 var router = express.Router();
 // const async = require('hbs/lib/async');
 const db = require('../config/db-connect');
 db.connect();
 
 /* GET users listing. */
-router.get('/',(req, res) =>{
-  res.render('users');
+router.get('/',async(req, res) =>{
+  let products = await db.get().collection("items").find().toArray();
+
+  res.render('users',{style:'users.css',products});
 });
 
 
